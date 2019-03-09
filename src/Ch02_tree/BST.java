b723d6a3553ade553b3e74f4f95cbf70873b15c8
= <wzjin85@foxmail.com>
2019-03-09 15:34:11 +0800

    add-ds: BST, including add/getMin/getMax/remove/contains/traverse

--- src/Ch02_tree/BST.java
+++ src/Ch02_tree/BST.java
@@ -1,5 +1,10 @@
 package Ch02_tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+import java.util.Stack;
+
 /**
  * @program: data-structure
  * @description: 二分搜索树
@@ -88,6 +93,11 @@ public class BST<E extends Comparable<E>> {
 		return node;
 	}
 
+	//返回元素是否在该树里
+	public boolean contains(E e) {
+		return contains(root, e);
+	}
+
 	//树中是否包含元素e，递归算法
 	private boolean contains(Node node, E e) {
 
@@ -101,6 +111,257 @@ public class BST<E extends Comparable<E>> {
 		} else {
 			return contains(node.right, e);
 		}
+
+	}
+
+	//BST的先序遍历
+	public void preOrder() {
+		preOrder(root);
+	}
+
+	//先序遍历以node为根的BST，递归
+	private void preOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		System.out.println(node.e);
+		preOrder(node.left);
+		preOrder(node.right);
+	}
+
+	//先序遍历以node为根的BST，非递归（使用栈）
+	public void preOrderNR() {
+		Stack<Node> stack = new Stack<>();
+		stack.push(root);
+		while(!stack.isEmpty()) {
+			Node curr = stack.pop();
+			System.out.println(curr.e);
+
+			if(curr.right != null) {
+				stack.push(curr.right);
+			}
+			if(curr.left != null) {
+				stack.push(curr.left);
+			}
+		}
+	}
+
+	//BST的中序遍历
+	public void inOrder() {
+		inOrder(root);
+	}
+
+	//中序遍历以node为根的BST，递归
+	private void inOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+
+		inOrder(node.left);
+		System.out.println(node.e);
+		inOrder(node.right);
+	}
+
+	//BST的中序遍历
+	public void postOrder() {
+		postOrder(root);
+	}
+
+	//中序遍历以node为根的BST，递归
+	private void postOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+
+		postOrder(node.left);
+		postOrder(node.right);
+		System.out.println(node.e);
+	}
+
+	//BST的层序遍历，借助queue
+	public void levelOrder() {
+		Queue<Node> q = new LinkedList<>();
+		q.add(root);
+		while(!q.isEmpty()) {
+			Node curr = q.remove();
+			System.out.println(curr.e);
+
+			if(curr.left != null) {
+				q.add(curr.left);
+			}
+			if(curr.right != null) {
+				q.add(curr.right);
+			}
+		}
+	}
+
+	//寻找BST中的最小值
+	public E getMin() {
+		if(size == 0) {
+			throw new IllegalArgumentException("BST is empty");
+		}
+
+		return getMin(root).e;
+	}
+
+	//返回以node为根的二分搜索树的最小值所在的节点
+	private Node getMin(Node node) {
+		if(node.left == null) {
+			return node;
+		}
+		return getMin(node.left);
+	}
+
+	//寻找BST中的最大值
+	public E getMax() {
+		return getMax(root).e;
+	}
+
+	//返回以node为根的二分搜索树的最大值所在的节点
+	private Node getMax(Node node) {
+		if(node.right == null) {
+			return node;
+		}
+		return getMax(node.right);
+	}
+
+	//从BST中删除最小值所在节点，返回最小值
+	public E removeMin() {
+		E ret = getMin();
+		root = removeMin(root);
+		return ret;
+	}
+
+	//删除以node为根的二分搜索树中的最小节点
+	//返回删除节点后新的二分搜索树的根
+	private Node removeMin(Node node) {
+
+		if(node.left == null) {
+			Node rightNode = node.right;
+			node.right = null;
+			size --;
+			return rightNode;
+		}
+		node.left = removeMin(node.left);
+		return node;
+	}
+
+	//从BST中删除最大值所在节点，返回最大值
+	public E removeMax() {
+		E ret = getMax();
+		root = removeMax(root);
+		return ret;
+	}
+
+	private Node removeMax(Node node) {
+		if(node.right == null) {
+			Node leftNode = node.left;
+			node.left = null;
+			size --;
+			return leftNode;
+		}
+		node.right = removeMax(node.right);
+		return node;
+	}
+
+	//从BST中删除元素为e的节点
+	public void remove(E e) {
+		root = remove(root ,e);
+	}
+
+	private Node remove(Node node, E e) {
+		if(node == null) {
+			return null;
+		}
+
+		if(e.compareTo(node.e) < 0) {
+			node.left = remove(node.left, e);
+			return node;
+		} else if (e.compareTo(node.e) > 0){
+			node.right = remove(node.right, e);
+			return node;
+		} else {
+			//找到要删除的目标节点
+			if(node.left == null) {
+				//目标节点的没有左子树
+				Node rightNode = node.right;
+				node.right = null;
+				size --;
+				return rightNode;
+			}
+			if(node.right == null) {
+				//目标节点没有右子树
+				Node leftNode = node.left;
+				node.left = null;
+				size --;
+				return leftNode;
+			}
+			//目标节点左右子树均不为空
+			//寻找比目标节点大的最小值，接替其位置
+			Node successor = getMin(node.right);
+			successor.right = removeMin(node.right);
+			successor.left = node.left;
+
+			//销毁旧node的左右节点
+			node.left = node.right = null;
+			//这里不需要size--：在removeMin操作的时候，执行了一次size--，
+			//但是这一次的操作是不对的，因为我们已经使用successor承接了min节点
+			//不是要将它删除，按照逻辑应该是size++，但在最后删除旧节点的时候，还是要size--，
+			//这样下来就抵消了。
+			return successor;
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder();
+		generateBSTString(root, 0, res);
+		return res.toString();
+	}
+
+	//生成以node为根节点，深度为depth的描述二叉树的字符串
+	private void generateBSTString(Node node, int depth, StringBuilder res) {
+		if(node == null) {
+			res.append(generateDepthString(depth)+"null\n");
+			return;
+		}
+		res.append(generateDepthString(depth)+node.e+"\n");
+		generateBSTString(node.left, depth+1, res);
+		generateBSTString(node.right, depth + 1, res);
+	}
+
+	private String generateDepthString(int depth) {
+		StringBuilder res = new StringBuilder();
+		for (int i = 0; i < depth; i++) {
+			res.append("--");
+		}
+		return res.toString();
+	}
+
+	public static void main(String[] args) {
+		BST<Integer> bst = new BST<>();
+		Random random = new Random();
+
+		int n = 10;
+
+		for (int i = 0; i < n; i++) {
+			bst.add(random.nextInt(100));
+		}
+		System.out.println(bst);
+
+//		System.out.println(bst.getMax());
+//		System.out.println(bst.getMin());
+//		bst.preOrder();
+//		System.out.println("-------------");
+//		bst.preOrderNR();
+//		System.out.println(bst);
+//		System.out.println("-------------");
+		bst.inOrder();
+//		System.out.println("-------------");
+//		bst.postOrder();
+//		System.out.println(bst);
+//		bst.levelOrder();
+
 
 	}
 
--- src/Ch02_tree/tree.md
+++ src/Ch02_tree/tree.md
@@ -13,3 +13,63 @@
    - 满二叉树：对于每一个非叶子结点都有两个结点。  
    - 二分搜索树(BST)：对于每个结点的值，都大于其左子树的所有结点的值，小于其右子树所有结点的值。  
         - 存储的元素必须是可比较的；
+        
+2. BST的元素的操作  
+    - 添加：元素可比较
+    - 包含
+    - 遍历(广度/深度):
+        1. 先序遍历(递归):自然且常用  
+            ```java
+            function traverse(node) {
+               if(node == null) {
+                   return;
+               }
+               //访问该节点
+               traverse(node.left);
+               traverse(node.right);
+            }
+            ```
+            非递归:利用栈LIFO的特性，将元素入栈（大的先入）
+            ```java
+            
+            ```
+        2. 中序遍历(递归):遍历的结果是顺序的  
+            ```java
+            function traverse(node) {
+               if(node == null) {
+                   return;
+               }
+               traverse(node.left);
+               //访问该节点
+               traverse(node.right);
+            }
+            ```
+        3. 后序遍历(递归)
+            ```java
+            function traverse(node) {
+               if(node == null) {
+                   return;
+               }
+               traverse(node.left);
+               //访问该节点
+               traverse(node.right);
+            }
+            ```
+        4. 层序遍历：借助队列
+        
+    - 删除：
+        1. 简单情况：要被删除的节点只有左子树或右子树
+        2. 复杂情况：要被删除的节点同时有左子树和右子树
+            - 设目标节点为d
+            - 找到值最接近d的节点s = min(d->right)
+            - 让s接替d的位置，删除d，s作为新的子树的根
+                - s->right = removeMin(d->right)
+                - s->left = d->left  
+                注：也可让s=max(d->left)来接替d的位置
+                
+    - 其他操作
+        - BST的floor/ceil
+        - BST的rank/select
+        - 维护size的BST
+        - 维护depth的BST
+        - 支持重复的BST（树中节点重复，使用count变量）
