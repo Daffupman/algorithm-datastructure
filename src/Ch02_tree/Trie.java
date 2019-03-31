a55902501bd9f71febddbf57a6058bfa2d214767
= <wzjin85@foxmail.com>
2019-03-31 16:35:31 +0800

    Trieç与字符串映射°

---
+++ src/Ch02_tree/Trie.java
@@ -0,0 +1,10 @@
+package Ch02_tree;
+
+/**
+ * @program: data-structure
+ * @description: 字典树/前缀树
+ * @author: Daffupman
+ * @create: 2019-03-30 15:11
+ **/
+public class Trie {
+}
---
+++ src/Ch02_tree/WordDictionary.java
@@ -0,0 +1,10 @@
+package Ch02_tree;
+
+/**
+ * @program: data-structure
+ * @description:
+ * @author: Daffupman
+ * @create: 2019-03-31 14:49
+ **/
+public class WordDictionary {
+}
