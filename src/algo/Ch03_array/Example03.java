27324ea7931e6208585215e5a4ef44e5cd4b117e
Daffupman <wzjin85@foxmail.com>
2019-07-28 12:21:48 +0800

    slide window and examples.

---
+++ src/algo/Ch03_array/Example03.java
@@ -0,0 +1,38 @@
+package algo.Ch03_array;
+
+/**
+ * @author Daffupman
+ * @description leetcode:209 Minimum Size Subarray Sum, 滑动窗口
+ * @date 2019/7/28 11:24
+ */
+public class Example03 {
+
+	public int minSubArrayLen(int s, int[] nums) {
+		//[l...r]为滑动窗口
+		int l = 0, r = -1;  //初始化时，滑动窗口里是没有元素的
+		int sum = 0;        //记录当前滑动窗口内的元素总和
+		int res = nums.length+1;    //记录当前寻找到的最小连续子数组的长度
+
+		while(l < nums.length) {
+			if( r+1 < nums.length && sum < s) {
+				sum += nums[++r];
+			}else{
+				sum -= nums[l++];
+			}
+
+			if(sum >= s) {
+				res = res <= r-l+1 ? res : r-l+1;
+			}
+		}
+		if(res == nums.length+1) {
+			return 0;
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {2,3,1,2,4,3};
+		int s = 7;
+	}
+
+}
---
+++ src/algo/Ch03_array/Example04.java
@@ -0,0 +1,34 @@
+package algo.Ch03_array;
+
+/**
+ * @author Daffupman
+ * @description leetcode:3 Longest Substring Without Repeating Character
+ *              滑动窗口
+ * @date 2019/7/28 11:48
+ */
+public class Example04 {
+
+	public int lengthOfLongestSubstring(String s) {
+		//申请一个256大小的数组记录字符是否重复（ASCII码），
+		boolean[] repeat = new boolean[256];
+		int l = 0, r = -1;      //滑动窗口为[l...r]
+		int res = 0;            //记录最长的滑动窗口大小
+
+		while(l < s.length()) {
+			if(r + 1 < s.length() && !repeat[s.charAt(r+1)]) {
+				//只要滑动窗口右界的下一个元素不是重复的，就加入窗口内，同时该字符标记为重复
+				repeat[s.charAt(++r)] = true;
+			} else {
+
+				repeat[s.charAt(l++)] = false;
+			}
+
+			//更新最大滑动窗口的大小
+			res = r-l+1 > res ? res : r-l+1;
+ 		}
+
+		return res;
+	}
+
+
+}
