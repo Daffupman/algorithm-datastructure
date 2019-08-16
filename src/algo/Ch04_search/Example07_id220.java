3722df80d0ae24ec71e7e88ae8b18d1cd4929a30
Daffupman <wzjin85@foxmail.com>
2019-08-16 20:22:47 +0800

    no idea

--- src/algo/Ch04_search/Example07_id220.java
+++ src/algo/Ch04_search/Example07_id220.java
@@ -16,23 +16,19 @@ public class Example07_id220 {
 		// 这个问题的测试数据在使用int进行加减运算时会溢出
 		// 所以使用long long
 		TreeSet<Long> record = new TreeSet<>();
-		for(int i = 0 ; i < nums.length ; i ++){
+		for (int i = 0; i < nums.length; i++) {
 
 			Long ceil = record.ceiling((long) nums[i] - (long) t);
-			if(ceil != null && ceil <= (long)nums[i] + (long)t)
+			if (ceil != null && ceil <= (long) nums[i] + (long) t)
 				return true;
 
-			record.add((long)nums[i]);
+			record.add((long) nums[i]);
 
-			if(record.size() == k + 1)
-				record.remove((long)nums[i-k]);
+			if (record.size() == k + 1)
+				record.remove((long) nums[i - k]);
 		}
 
 		return false;
-	}
-
-	private static void printBool(boolean b){
-		System.out.println(b ? "True" : "False");
 	}
 
 }
