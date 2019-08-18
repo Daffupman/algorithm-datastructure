9706e73a45281cec13402b6c3ea47a8724d20c9a
Daffupman <wzjin85@foxmail.com>
2019-08-18 15:20:34 +0800

    detailed。GET！

--- src/algo/Ch06_stack_queue/Example02_id144.java
+++ src/algo/Ch06_stack_queue/Example02_id144.java
@@ -47,6 +47,23 @@ public class Example02_id144 {
 		}
 	}
 
+	/**
+	 * 另一种非递归的写法，对经典写法改造：
+	 * 使用栈模拟系统栈，把原本应该由系统将当前程序的执行状态压入系统栈中
+	 * 转变成使用自己创建的栈保存系统需要保存的程序状态。
+	 *
+	 * 所以需要封装一个Command类来保存节点及其状态（描述信息）
+	 * 其中描述信息s， 其取值为go或print，代表对当前节点的指令描述
+	 * 对于每一个节点的初始指令描述为go，当从栈中取出之后，指令变更为print
+	 *
+	 * 大体思路与经典的一致：
+	 * 先将root压栈，接着一直监听stack的是否为空。
+	 * 如果不为空，则
+	 * 		- 判断当前节点的状态
+	 * 			- go：前寻指令
+	 * 			- print：访问指令
+	 * 栈空，结束遍历的过程
+	 */
 	public ArrayList<Integer> preorderTraversal2(TreeNode root) {
 		ArrayList<Integer> res = new ArrayList<>();
 		if( root == null ) {
@@ -59,10 +76,13 @@ public class Example02_id144 {
 			Command cmd = stack.pop();
 
 			if( "print".equals( cmd.s ) ) {
+				// 专注访问的分支
 				res.add( cmd.node.val );
 			} else {
+				// 专注前寻的分支
 				assert "go".equals(cmd.s);
-				//核心过程,改动go和print的入栈顺序即可完成先、中、后序遍历
+
+				//===核心过程,改动go和print的入栈顺序即可完成先、中、后序遍历====
 				if( cmd.node.right != null ) {
 					stack.push( new Command("go", cmd.node.right) );
 				}
@@ -70,6 +90,7 @@ public class Example02_id144 {
 					stack.push( new Command( "go", cmd.node.left ) );
 				}
 				stack.push( new Command( "print", cmd.node ) );
+				//======================================================
 			}
 		}
 		return res;
