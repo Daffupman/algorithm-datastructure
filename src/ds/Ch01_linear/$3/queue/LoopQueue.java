5a80f224b8b3f38b9d39e5c6918f1b061b4be820
Daffupman <wzjin85@foxmail.com>
2019-08-21 16:15:27 +0800

    bug fix.

--- src/ds/Ch01_linear/$3/queue/LoopQueue.java
+++ src/ds/Ch01_linear/$3/queue/LoopQueue.java
@@ -5,10 +5,10 @@ import ds.Ch01_linear.$3.queue.impl.LinkedListQueue;
 /**
  * @description 循环队列
  * 				1.生成的数组总是会多申请一个单位的空间，用于队满检查
- * 				2.队空：front == tail;
- * 				3.队满：(tail+1)%data.length == front;
+ * 				2.队空：front == rear;
+ * 				3.队满：(rear+1)%data.length == front;
  * 				4.入队：front = (front+1)%data.length;
- * 				5.出队：tail = (tail+1)%data.length;
+ * 				5.出队：rear = (rear+1)%data.length;
  * @author Daffupman
  * @date 2019/07/08
  */
@@ -16,8 +16,8 @@ public class LoopQueue<E> implements Queue<E> {
 
 	private E[] data;
 	private int size;
-	private int front,tail;
-	
+	private int front, rear;
+
 	public LoopQueue() {
 		this(10);
 	}
@@ -26,7 +26,7 @@ public class LoopQueue<E> implements Queue<E> {
 		data = (E[])new Object[capacity+1];
 		size = 0;
 		front = 0;
-		tail = 0;
+		rear = 0;
 	}
 	
 	//将数组调整为指定大小的容量
@@ -34,9 +34,13 @@ public class LoopQueue<E> implements Queue<E> {
 		E[] newData = (E[])new Object[newCapacity+1];
 		//数据迁移
 		for (int i = 0; i < size; i++) {
-			newData[i] = data[i];
+			newData[i] = data[(i+front)%data.length];
 		}
 		data = newData;
+	}
+
+	private int getCapacity() {
+		return data.length;
 	}
 	
 	@Override
@@ -46,23 +50,34 @@ public class LoopQueue<E> implements Queue<E> {
 
 	@Override
 	public boolean isEmpty() {
-		return size == 0;
+		return front == rear;
 	}
 
 	@Override
 	public void enqueue(E e) {
-		if((tail+1)%data.length == front) {
+		if((rear +1)%getCapacity() == front) {
 			resize(2 * size);
 		}
-		data[tail] = e;
-		tail = (tail+1)%data.length;
+		data[rear] = e;
+		rear = (rear +1)%getCapacity();
+		size ++;
 	}
 
 	@Override
 	public E dequeue() {
+		if(isEmpty()) {
+			throw new IllegalArgumentException("Empty Queue!");
+		}
+
 		E ret = data[front];
 		data[front] = null;
-		front = (front+1)%data.length;
+		front = (front+1)%getCapacity();
+		size --;
+
+		if(size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+			resize(getCapacity() / 2);
+		}
+
 		return ret;
 	}
 
@@ -70,9 +85,23 @@ public class LoopQueue<E> implements Queue<E> {
 	public E getFront() {
 		return data[front];
 	}
-	
+
+	@Override
+	public String toString() {
+		StringBuilder res = new StringBuilder(50);
+		res.append("Queue: [");
+		for(int i = front; i != rear; i = (i+1)%getCapacity()) {
+			res.append(data[i]);
+			if((i+1)%getCapacity() != rear) {
+				res.append(", ");
+			}
+		}
+		res.append("]");
+		return res.toString();
+	}
+
 	public static void main(String[] args) {
-		Queue<Integer> queue = new LinkedListQueue<>();
+		LoopQueue<Integer> queue = new LoopQueue<>();
 		System.out.println(queue);
 		for (int i = 0; i < 5; i++) {
 			queue.enqueue(i);
